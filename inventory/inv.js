27533f4fbaf173f65452975211c5da9c0d2b011c
ari5av <ari5av@gmail.com>
2018-11-28 03:04:16 -0500

    made inventory work with localStorage

--- inventory/inv.js
+++ inventory/inv.js
@@ -2,12 +2,88 @@ $(document).ready(function() {
 
   Inventory = {
     INV_FIELDS: _.size($('.inv-field')),
+    items: [],
+    money: {'cp':0,'sp':0,'gp':0,'pp':0,'ep':0,'other':''},
 
     save_inventory: function() {
-      console.log('saved!');
-      return false;
+      for (i=1;i<=Inventory.INV_FIELDS;i++) {
+        Inventory.items[i] = {
+          'count': $('#count-'+i).val(),
+          'desc': $('#desc-'+i).val(),
+          'weight': $('#weight-'+i).val()
+        };
+      }
+
+      Inventory.money = {
+        'cp': $('#cp').val(),
+        'sp': $('#sp').val(),
+        'gp': $('#gp').val(),
+        'pp': $('#pp').val(),
+        'ep': $('#ep').val(),
+        'other': $('#other-wealth').val()
+      };
+
+      try {
+        window.localStorage.setItem('items', JSON.stringify(Inventory.items));
+        window.localStorage.setItem('money', JSON.stringify(Inventory.money));
+        return true;
+      } catch (err) {
+        console.log("couldn't save: " + err);
+        return false;
+      }
+    },
+
+    load_inventory: function() {
+      try {
+        Inventory.items = JSON.parse(window.localStorage.getItem('items'));
+        Inventory.money = JSON.parse(window.localStorage.getItem('money'));
+        if (Inventory.items === null) {
+          Inventory.items = [];
+        }
+        if (Inventory.money === null) {
+          Inventory.money = {'cp':0,'sp':0,'gp':0,'pp':0,'ep':0,'other':''};
+        }
+
+        for (i=1;i<=Inventory.INV_FIELDS;i++) {
+          $('#count-'+i).val(Inventory.items[i].count);
+          $('#desc-'+i).val(Inventory.items[i].desc);
+          $('#weight-'+i).val(Inventory.items[i].weight);
+        }
+        $('#cp').val(Inventory.money.cp);
+        $('#sp').val(Inventory.money.sp);
+        $('#gp').val(Inventory.money.gp);
+        $('#pp').val(Inventory.money.pp);
+        $('#ep').val(Inventory.money.ep);
+        $('#other-wealth').text(Inventory.money.other);
+
+        
+      } catch (err) {
+        Inventory.items = [];
+        Inventory.money = {'cp':0,'sp':0,'gp':0,'pp':0,'ep':0,'other':''};
+        console.log('couldn\'t load: ' + err);
+      }
+    },
+
+    clear_inventory = function() {
+      for (i=1;i<=Inventory.INV_FIELDS;i++) {
+        $('#count-'+i).val('');
+        $('#desc-'+i).val('');
+        $('#weight-'+i).val('');
+      }
+      $('#cp').val('');
+      $('#sp').val('');
+      $('#gp').val('');
+      $('#pp').val('');
+      $('#ep').val('');
+      $('#other-wealth').text('');
     }
+        
   };
 
-  $('#save').click(Inventory.save_inventory);
+  Inventory.load_inventory();
+
+  $('#save-top').click(Inventory.save_inventory);
+  $('#save-bottom').click(Inventory.save_inventory);
+  $('#clear-top').click(Inventory.clear_inventory);
+  $('#clear-bottom').click(Inventory.clear_inventory);
 });
